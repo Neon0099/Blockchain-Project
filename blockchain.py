2775b1513228ea030ed249f9539f8ff7c6745e63
Neon_99 <f20210896@hyderabad.bits-pilani.ac.in>
2024-10-07 22:12:33 +0530

    Added some changes

--- blockchain.py
+++ blockchain.py
@@ -8,45 +8,14 @@ class Blockchain:
     def __init__(self):
         self.current_transactions = []
         self.chain = []
-        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash='1', proof=100)
 
-    def register_node(self, address):
-        """
-        Add a new node to the list of nodes
-        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
-        """
-        self.nodes.add(address)
-
-    def valid_chain(self, chain):
-        """
-        Determine if a given blockchain is valid
-        :param chain: A blockchain
-        :return: True if valid, False if not
-        """
-
-        last_block = chain[0]
-        current_index = 1
-
-        while current_index < len(chain):
-            block = chain[current_index]
-            if block['previous_hash'] != self.hash(last_block):
-                return False
-
-            if not self.valid_proof(last_block['proof'], block['proof'], last_block['previous_hash']):
-                return False
-
-            last_block = block
-            current_index += 1
-
-        return True
-
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
-        :param proof: The proof given by the Proof of Work algorithm
+        :param proof: The proof given by the Proof of Work algorithm (nonce)
         :param previous_hash: Hash of previous Block
         :return: New Block
         """
@@ -55,13 +24,18 @@ class Blockchain:
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'proof': proof,  # This is the nonce
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),  # Hash of the previous block
+            'current_hash': None  # Placeholder for the current hash
         }
+
+        # Calculate and store the current block's hash
+        block['current_hash'] = self.hash(block)
 
         # Reset the current list of transactions
         self.current_transactions = []
 
+        # Add the new block to the chain
         self.chain.append(block)
         return block
 
@@ -83,6 +57,9 @@ class Blockchain:
 
     @property
     def last_block(self):
+        """
+        Returns the last block in the chain
+        """
         return self.chain[-1]
 
     @staticmethod
@@ -90,25 +67,25 @@ class Blockchain:
         """
         Creates a SHA-256 hash of a Block
         :param block: Block
+        :return: <str> Hash of the block
         """
-
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_block):
         """
         Simple Proof of Work Algorithm:
-         - Find a number p' such that hash(pp') contains leading 4 zeroes
-         - Where p is the previous proof, and p' is the new proof
+        - Find a number p' such that hash(pp') contains leading 2 zeroes
+        - Where p is the previous proof, and p' is the new proof
         :param last_block: <dict> last Block
-        :return: <int>
+        :return: <int> Proof (nonce)
         """
 
         last_proof = last_block['proof']
         last_hash = self.hash(last_block)
 
         proof = 0
-        while self.valid_proof(last_proof, proof, last_hash) is False:
+        while not self.valid_proof(last_proof, proof, last_hash):
             proof += 1
 
         return proof
@@ -116,7 +93,7 @@ class Blockchain:
     @staticmethod
     def valid_proof(last_proof, proof, last_hash):
         """
-        Validates the Proof
+        Validates the Proof: Does hash(last_proof, proof, last_hash) contain 2 leading zeroes?
         :param last_proof: <int> Previous Proof
         :param proof: <int> Current Proof
         :param last_hash: <str> The hash of the Previous Block
@@ -125,11 +102,36 @@ class Blockchain:
 
         guess = f'{last_proof}{proof}{last_hash}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        return guess_hash[:2] == "00"  # Check if it starts with two zeroes
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: A blockchain
+        :return: True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+
+            # Check that the previous hash is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block['current_hash']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
 
 
-# Instantiate the Blockchain
-blockchain = Blockchain()
+# User Interaction Section
 
 def print_menu():
     print("\nBlockchain Menu:")
@@ -138,18 +140,34 @@ def print_menu():
     print("3. Display the blockchain")
     print("4. Exit")
 
-def mine_block():
+
+def mine_block(blockchain):
+    """
+    Function to mine a new block
+    """
+    if not blockchain.current_transactions:
+        print("No transactions to mine.")
+        return
+
     last_block = blockchain.last_block
     proof = blockchain.proof_of_work(last_block)
+
+    # Miner receives a reward for finding the proof
     blockchain.new_transaction(
         sender="0",  # Reward for mining
         recipient=str(uuid4()).replace('-', ''),
         amount=1,
     )
+
+    # Forge the new block
     block = blockchain.new_block(proof)
     print(f"New block mined! Block index: {block['index']}")
 
-def add_transaction():
+
+def add_transaction(blockchain):
+    """
+    Function to add a new transaction
+    """
     sender = input("Enter the sender: ")
     recipient = input("Enter the recipient: ")
     amount = input("Enter the amount: ")
@@ -157,22 +175,29 @@ def add_transaction():
     index = blockchain.new_transaction(sender, recipient, float(amount))
     print(f"Transaction will be added to block {index}")
 
-def display_chain():
+
+def display_chain(blockchain):
+    """
+    Function to display the entire blockchain
+    """
     for block in blockchain.chain:
         print(json.dumps(block, indent=4))
 
 
 if __name__ == "__main__":
+    # Instantiate the Blockchain
+    blockchain = Blockchain()
+
     while True:
         print_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            mine_block()
+            mine_block(blockchain)
         elif choice == '2':
-            add_transaction()
+            add_transaction(blockchain)
         elif choice == '3':
-            display_chain()
+            display_chain(blockchain)
         elif choice == '4':
             print("Exiting...")
             break
